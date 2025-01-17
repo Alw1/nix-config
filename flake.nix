001ee2dca89c33fa140aa95ef2a0a119cfafd164
Alex Wyatt <alexander@jdwyatt.com>
2025-01-17 13:25:46 -0500

    Testing

--- flake.nix
+++ flake.nix
@@ -1,15 +1,27 @@
 {
+  description = "A modular NixOS flake for multiple hosts";
+
   inputs = {
- 	 nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
-	#	 home_manager.url =  "https://github.com/nix-community/home-manager/archive/release-24.05.tar.gz home-manager";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";  # You are using NixOS 24.11 (which is fine)
+    flake-utils.url = "github:numtide/flake-utils";  # Use flake-utils for multi-system support
   };
-  outputs = inputs@{ self, nixpkgs, ... }: {
-    # NOTE: 'nix' on the next line is the hostname 
-    nixosConfigurations = {
-		toaster = nixpkgs.lib.nixosSystem {
-		  system = "x86_64-linux";
-		  modules = [ ./hosts/toaster ];
-		};
-	};
-  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachSystem [ "x86_64-linux" ] (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [];  # Add your overlays here if needed
+      };
+
+      toasterConfig = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";  
+        modules = [ ./hosts/toaster ];  
+      };
+
+    in {
+      nixosConfigurations = {
+        toaster = toasterConfig;  
+      };
+    });
 }
+
